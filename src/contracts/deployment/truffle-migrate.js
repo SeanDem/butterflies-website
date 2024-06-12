0a056dc863ddcaf90fb4b81d92ccd846ab684809
Sean Demeritt <seandemeritt13@gmail.com>
2024-06-11 23:37:45 -0400

    fix

--- src/contracts/deployment/truffle-migrate.js
+++ src/contracts/deployment/truffle-migrate.js
@@ -1,5 +1,5 @@
 const BlockchainButterflies = artifacts.require("BlockchainButterflies");
 
 module.exports = function(deployer) {
-  deployer.deploy(BlockchainButterflies, "http://mybaseuri.com/");
+  deployer.deploy(BlockchainButterflies, "http://0xC2DB2fA3f5D21D8aE1376cE1d560FCC6F3EE4c41.com/");
 };
